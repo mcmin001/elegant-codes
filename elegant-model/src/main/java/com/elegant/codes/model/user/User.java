bb355b4df54fe54810337b525e2081c85249b205
mcmin001 <mcmin001@sina.com>
2021-09-14 23:49:26 +0800

    提交测试类

--- elegant-model/src/main/java/com/elegant/codes/model/user/User.java
+++ elegant-model/src/main/java/com/elegant/codes/model/user/User.java
@@ -5,4 +5,50 @@ import java.io.Serializable;
 
 public class User implements Serializable {
     private static final long serialVersionUID = 153978162555185435L;
+
+    private String userId;
+    private String userName;
+    private String gender;
+    private int age;
+    private String birthday;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 }
